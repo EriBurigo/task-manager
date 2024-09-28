17d8d7b7365b3448249442c409bf9bc8a2a10d89
Eriton da Silva Búrigo <eritonsburigo@gmail.com>
2024-09-27 23:52:27 -0300

    Adicione validação para evitar a configuração de alarmes no passado

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :alarm_time, presence: false # O alarme é opcional
+  validate :alarm_time_cannot_be_in_the_past
+
+  def alarm_time_cannot_be_in_the_past
+    if alarm_time.present? && alarm_time < Time.now
+      errors.add(:alarm_time, "não pode estar no passado")
+    end
+  end
 end
