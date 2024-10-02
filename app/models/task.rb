a722a320a16184d07c43b55309ae03c2341682e7
Eriton da Silva Búrigo <eritonsburigo@gmail.com>
2024-10-02 16:15:34 -0300

    Adiciona método para verificar os próximos alarmes nas tarefas

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,11 @@ class Task < ApplicationRecord
       errors.add(:alarm_time, "não pode estar no passado")
     end
   end
+
+  def self.check_for_upcoming_alarms
+    Task.where('alarm_time <= ?', Time.now + 5.minutes).each do |task|
+      # Enviar notificação ou exibir log no console
+      puts "Alarme para a tarefa: #{task.name} está próximo!"
+    end
+  end  
 end
