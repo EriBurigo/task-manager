434350f3eb13a8626b0bcc7cbe876656f989dbdf
Eriton da Silva Búrigo <eritonsburigo@gmail.com>
2024-10-12 16:08:07 -0300

    Adiciona uma nova migração para poder arquivar as tarefas

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_23_214721) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_12_190434) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "alarm_time"
+    t.boolean "archived", default: false
   end
 
 end
