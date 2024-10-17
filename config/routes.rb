2764757b6bd9620a2f521eb688dd49474ef1e963
Eriton da Silva Búrigo <eritonsburigo@gmail.com>
2024-10-16 22:13:29 -0300

    Define uma rota para chamar o metodo completed

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
       patch :complete
       patch :archive  # Adiciona essa rota para arquivar uma tarefa específica
     end
+
+    resources :tasks do
+      patch :complete, on: :member
+    end    
   end
 end
